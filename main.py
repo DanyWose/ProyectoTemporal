da36c7e27ffb21637747e3b230da7357330a0ed9
DanyWose <danystorm22@gmail.com>
2023-02-20 21:37:53 +0100

    Añade main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame, sys
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+FPS = 60
+
+WIDTH, HEIGHT = 900, 600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Proyecto😳")
+
+
+def main():
+    
+    clock = pygame.time.Clock()
+
+    while True:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
